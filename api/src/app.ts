d5e8f8f6eb2d3e2b22def729c07057ec28618a3b
Gueka <marcojcapo@gmail.com>
2021-01-23 16:21:06 -0500

    Code comments added

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,43 +2,68 @@
 import express from 'express';
 import { Store } from './service/store';
 
+/*
+  This app typescript file was create as an example of an CRUD (Create, Read, Update and Delete) API build with nodejs using expressjs.
+  If you are familiar with postman you can import the postman_collection.json found in the docs folder,
+  that way you will found much easier to test some request examples for this API.
+
+  This API is an example of a backend service, imagine the requirement for a simple web page that shows articles as cards,
+  it could contain a title, a description, a link to the article, and in the future an image.
+
+  I wrote an article about this process and you can find it here: https://medium.com/p/93049979f1aa
+*/
+
 const app = express();
 const port = 3000;
+// Initialize of Store service singleton
 const store = Store.getInstance();
 
 // Use express json middleware so it transform the request into json.
 app.use(express.json());
 
-
+// Simple test method PING to make sure the API is working
 app.get('/ping', (req, res) => {
   res.send({code:200, message:'Pong!'});
 });
 
+// Using method .get we are able to expose a GET API to retrieve an article by it's id.
+// Web browser use GET method to open web pages. Ex: http://localhost:3000/article/379acb20-fe70-4ecc-a814-633c587df0b7
 app.get('/article/:id', (req, res) => {
   let id = req.params.id;
   console.log(`Getting article with id ${id}`);
+  // Use the store service to retrieve the article requested.
   let article = store.getArticle(id);
+  // Validate if the article exist
   if(article == undefined){
     console.warn("Request id not found.");
+    // status is used to set the http status code to be return
+    // send will send any json formatted response
     res.status(404).send({status: 404, message:`Article with id ${id} was not found.`});
   }else{
-    res.send(article);
+    // Return a 200 with the article.
+    res.send({status:200, message:`Article with id ${id} was found.`, article:article});
   }
 });
 
+// Expose post method to create a new article, a new ID will be generated
 app.post('/article', (req, res) => {
   let title = req.body.title;
   let description = req.body.description;
   let link = req.body.link;
+  // Send data to store new article in our service layer
   let newArticle = store.setArticle(title, description, link);
-
   if(newArticle != null){
+    // Successful article created, we return it with the id assigned
     res.send({code:200, message:`New article has been created.`, article: newArticle});
   }else{
+    // If any mandatory parameter is missing we return a 400 error
+    console.warn("Missing mandatory parameter.");
     res.status(400).send({code:400, message:'Missing a mandatory parameter: title, description or link'})
   }
 });
 
+// PUT method is used to update an existing article with the data provided.
+// only the id is mandatory, if the id is not found in the store it will return a 404 NOT_FOUND status code
 app.put('/article/:id', (req, res) => {
   let id = req.params.id;
   let title = req.body.title;
@@ -53,6 +78,8 @@ app.put('/article/:id', (req, res) => {
   }
 });
 
+// DELETE method is used to delete an existing article.
+// only the id is mandatory, if the id is not found in the store it will return a 404 NOT_FOUND status code
 app.delete('/article/:id', (req, res) => {
   let id = req.params.id;
   if(store.deleteArticle(id)){
@@ -62,7 +89,9 @@ app.delete('/article/:id', (req, res) => {
   }
 });
 
+// Start our web service to listen in the port provided
 app.listen(port, () => {
+  // You will find some logs with `` instead of "", this way I can use ${} to log variables
   return console.log(`Server is listening on ${port}`);
 });
 
--- api/src/service/store.ts
+++ api/src/service/store.ts
@@ -1,4 +1,8 @@
 
+
+/*
+    This is our model class Article, also know as definition or DTO object
+*/
 export class Article {
     id: string;
     title: string;
@@ -35,29 +39,39 @@ export class Article {
         this.link = link;
     }
 }
+// Library used to generate Universal unique identifiers. https://en.wikipedia.org/wiki/Universally_unique_identifier
 const { v4: uuidv4 } = require("uuid");
 
+/*
+    The Store class is using a design pattern call Singleton.
+    Singletons are used to have a single instance of this objects across all the application
+    It force to use the static method getInstance and hide it's contrutor so none can initialize another Store class (! new Store());
+    For more information about this pattern go here: https://en.wikipedia.org/wiki/Singleton_pattern
+
+    Instead of a map propery the proper thing to do would be to change the map to a database connection.
+*/
 export class Store {
 
     private static instance: Store;
+    // This is a very, very simple in memory store. https://en.wikipedia.org/wiki/Storage_(memory)
+    // In memory means that if the application is restarted or stopped, the data will be lost.
     private map = new Map();
 
-    private constructor() {
-    }
-  
+    private constructor() {}
     public static getInstance(): Store {
       if (!Store.instance) {
         Store.instance = new Store();
       }
-  
       return Store.instance;
     }
 
-
+    // Exposing a read method to retrieve an article
     public getArticle(id: string): Article{
         return this.map.get(id);
     }
     
+    // Exposing a store article method, some basic validation for mandatory parameters.
+    // If we have a database connection we should check if the characters are valid to avoid SQL injections https://en.wikipedia.org/wiki/SQL_injection
     public setArticle(title: string, description:string, link:string): Article {
         if(title == null || description == null || link == null){
             return null;
@@ -70,21 +84,25 @@ export class Store {
         return article;
     }
 
+    // Exposing a method to update an existing article properties
     updateArticle(id: string, title: string, description:string, link:string): boolean {
         let article = this.map.get(id);
-        if(title != undefined){
-            article.setTitle(title);
-        }
-        if(description != undefined){
-            article.setDescription(description);
-        }
-        if(link != undefined){
-            article.setLink(link);
+        if(article != null){
+            if(title != undefined){
+                article.setTitle(title);
+            }
+            if(description != undefined){
+                article.setDescription(description);
+            }
+            if(link != undefined){
+                article.setLink(link);
+            }
         }
 
         return article;
     }
 
+    // Exposing method to delete an existing article
     deleteArticle(id: string): boolean {
         if(this.map.has(id)){
             this.map.delete(id);
